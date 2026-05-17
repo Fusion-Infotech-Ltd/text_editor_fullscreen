36a1ed1cfea334838a4b1d913081d9b0661ac4b0
Raisul Islam <mechrsl0@gmail.com>
2026-05-17 13:37:39 +0600

    chore: transition transform effect

--- text_editor_fullscreen/hooks.py
+++ text_editor_fullscreen/hooks.py
@@ -25,8 +25,8 @@ app_license = "mit"
 # ------------------
 
 # include js, css files in header of desk.html
-# app_include_css = "/assets/text_editor_fullscreen/css/text_editor_fullscreen.css"
-# app_include_js = "/assets/text_editor_fullscreen/js/text_editor_fullscreen.js"
+app_include_css = "/assets/text_editor_fullscreen/css/text_editor_fullscreen.css"
+app_include_js = "/assets/text_editor_fullscreen/js/text_editor_fullscreen.js"
 
 # include js, css files in header of web template
 # web_include_css = "/assets/text_editor_fullscreen/css/text_editor_fullscreen.css"
